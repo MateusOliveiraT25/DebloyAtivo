1cfe1ff2f1ebfab3aed5150bbd95b0f51aadd148
Mateus Oliveira <123783867+MateusOliveiraT25@users.noreply.github.com>
2024-06-17 16:12:27 -0300

    Delete DebloyAtivos directory

--- DebloyAtivos/src/main/java/br/com/ativos/ativospatrimoniais/repository/AmbienteRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ativos.ativospatrimoniais.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.ativos.ativospatrimoniais.model.Ambientes;
-
-public interface AmbienteRepository extends JpaRepository<Ambientes, Long> {
-}
